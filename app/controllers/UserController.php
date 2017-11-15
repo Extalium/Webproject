2bc8e42a17d46c4ffe3917d1f5d7e513f5d275a6
Extalium <aug.leroy@gmail.com>
2017-11-15 11:53:10 +0100

    Login page MVC architecture

---
+++ app/controllers/UserController.php
@@ -0,0 +1,13 @@
+<?php
+
+class UserController extends Controller{
+	
+	function render() {
+		$template= new Template;
+		echo $template->render('login.htm');
+	}
+
+	function authenticate() {
+		/* ---------------TODO------------------*/
+	}
+}
---
+++ tmp/1rhmuqyegwskk.3g4prvj0at2c8.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Tutorial page</title>
+</head>
+<body>
+	<p>Login page </p>
+</body>
+</html>
