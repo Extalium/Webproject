1bb41e83b086a4dbce8b8b1b7f22c38017e14be3
Iloufilm <ilann.portugais@gmail.com>
2017-11-16 12:57:56 +0100

    Update UserController.php

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -9,5 +9,23 @@ class UserController extends Controller{
 
 	function authenticate() {
 		/* ---------------TODO------------------*/
+		$id=this->f3->get('POST.id');
+		$mdp=this->f3->get('POST.mdp');
+		
+		$the_user = new User($this->db);
+		$the_user->getbyName($id);
+		
+		if($the_user->dry())
+		{
+			this->f3->reroute->('/login');
+		}
+		
+		if(password_verify($mdp,$the_user->$mdp){
+			$this->f3->set('SESSION.the_user', $the_user->id);
+			$this->f3->reroute('/');
+		}
+		else{
+			$this->f3->reroute('/login');
+		}
 	}
 }
