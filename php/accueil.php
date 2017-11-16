776a033187e7ecc80d2dfc11913d49e8fb1e8974
alexandrelorange <lorange@et.esiea.fr>
2017-11-16 21:37:29 +0100

    add scroll bar
    la scroll bar fonctionne en front mais il reste a la rndre dynamique

--- php/accueil.php
+++ php/accueil.php
@@ -1,4 +1,4 @@
-<br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
+<br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><h1> bonjour</h1>
 <?php
 printf('bonjour');printf('bonjour');printf('bonjour');
 printf('bonjour');
